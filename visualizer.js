11901fa9cc9cc843d90ee480174e14bee6763b78
Robin Lake <robin@tempus-ex.com>
2022-05-19 12:58:39 -0700

    remove comments

--- visualizer.js
+++ visualizer.js
@@ -1,7 +1,4 @@
 const volume = document.getElementById('volume')
-// const bass = document.getElementById('bass')
-// const mid = document.getElementById('mid')
-// const treble = document.getElementById('treble')
 const visualizer = document.getElementById('visualizer')
 
 function drawVisualizer() {
