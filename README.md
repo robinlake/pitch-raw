67080ca91a974eb7fd5f2a60c032738f058397d1
Robin Lake <robinhlake@gmail.com>
2023-07-05 08:18:34 -0700

    restarted. Little readme note, uncommitted changes from over a year ago I guess?

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pitch-raw
+
+Originally made this repo in May 2022. Starting to use it for a new purpose Jul 2023. Wanted to keep it as a reminder of how hard things were back then and how much things have changed.
--- visualizer.js
+++ visualizer.js
@@ -1,60 +1,63 @@
 const volume = document.getElementById('volume')
 const visualizer = document.getElementById('visualizer')
 
-function drawVisualizer() {
-    requestAnimationFrame(drawVisualizer)
+// function drawVisualizer(analyserNode, context) {
+//     requestAnimationFrame(drawVisualizer)
 
   
-    const bufferLength = analyserNode.frequencyBinCount
+//     const bufferLength = analyserNode.frequencyBinCount
 
-    const maxFrequency = context.sampleRate;
-    const frequencyArray = new Array(bufferLength);
-    for (let i = 0; i < bufferLength; i++) {
-        frequencyArray[i] = ((i+1)/bufferLength) * maxFrequency;
-    }
+//     const maxFrequency = context.sampleRate;
+//     const frequencyArray = new Array(bufferLength);
+//     for (let i = 0; i < bufferLength; i++) {
+//         frequencyArray[i] = ((i+1)/bufferLength) * maxFrequency;
+//     }
 
-    const dataArray = new Uint8Array(bufferLength)
-    analyserNode.getByteFrequencyData(dataArray)
+//     const dataArray = new Uint8Array(bufferLength)
+//     analyserNode.getByteFrequencyData(dataArray)
 
-    const frequencyStrengthMap = new Map()
-    for (let i = 0; i < bufferLength; i++) {
-        frequencyStrengthMap.set(frequencyArray[i], dataArray[i]);
-    }
+//     const frequencyStrengthMap = new Map()
+//     for (let i = 0; i < bufferLength; i++) {
+//         frequencyStrengthMap.set(frequencyArray[i], dataArray[i]);
+//     }
 
-    sortedStrengths = new Map([...frequencyStrengthMap.entries()].sort((a, b) => b[1] - a[1]))
+//     let sortedStrengths = new Map([...frequencyStrengthMap.entries()].sort((a, b) => b[1] - a[1]))
 
-    if (dataArray[0] > 40) {
-        console.log("bufferLength: ", bufferLength);
-        console.log("frequencyArray: ", frequencyArray);
-        console.log("frequencyStrengthMap: ", frequencyStrengthMap);
-        console.log("sortedStrengths: ", sortedStrengths);
-        console.log(dataArray);
-    }
+//     if (dataArray[0] > 40) {
+//         console.log("bufferLength: ", bufferLength);
+//         console.log("frequencyArray: ", frequencyArray);
+//         console.log("frequencyStrengthMap: ", frequencyStrengthMap);
+//         console.log("sortedStrengths: ", sortedStrengths);
+//         console.log(dataArray);
+//     }
 
 
-    // const timeDomainArray = new Uint8Array(bufferLength)
-    // analyserNode.getByteTimeDomainData(timeDomainArray)
-    // console.log(timeDomainArray);
-    const width = visualizer.width
-    const height = visualizer.height
-    const barWidth = width / bufferLength
+//     // const timeDomainArray = new Uint8Array(bufferLength)
+//     // analyserNode.getByteTimeDomainData(timeDomainArray)
+//     // console.log(timeDomainArray);
+//     const width = visualizer.width
+//     const height = visualizer.height
+//     const barWidth = width / bufferLength
   
-    const canvasContext = visualizer.getContext('2d')
-    canvasContext.clearRect(0, 0, width, height)
+//     const canvasContext = visualizer.getContext('2d')
+//     canvasContext.clearRect(0, 0, width, height)
   
-    dataArray.forEach((item, index) => {
-      const y = item / 255 * height / 2
-      const x = barWidth * index
+//     dataArray.forEach((item, index) => {
+//       const y = item / 255 * height / 2
+//       const x = barWidth * index
   
-      canvasContext.fillStyle = `hsl(${y / height * 400}, 100%, 50%)`
-      canvasContext.fillRect(x, height - y, barWidth, y)
-      if (item > 10) {
-          canvasContext.strokeText(frequencyArray[index], x, height - y)
-      }
-    })
-  }
+//       canvasContext.fillStyle = `hsl(${y / height * 400}, 100%, 50%)`
+//       canvasContext.fillRect(x, height - y, barWidth, y)
+//       if (item > 10) {
+//           canvasContext.strokeText(frequencyArray[index], x, height - y)
+//       }
+//     })
+//   }
   
   function resize() {
     visualizer.width = visualizer.clientWidth * window.devicePixelRatio
     visualizer.height = visualizer.clientHeight * window.devicePixelRatio
   }
+
+  // export {volume, visualizer, drawVisualizer, resize}
+  export {volume, visualizer, resize}
