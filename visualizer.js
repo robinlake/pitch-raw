d8cd175373664f33be1b2c444df55984ee8d41b4
Robin Lake <robinhlake@gmail.com>
2023-07-06 10:17:18 -0700

    blatantly steal

--- visualizer.js
+++
@@ -1,8 +0,0 @@
-const volume = document.getElementById('volume')
-
-  function resize() {
-    visualizer.width = visualizer.clientWidth * window.devicePixelRatio
-    visualizer.height = visualizer.clientHeight * window.devicePixelRatio
-  }
-
-  export {volume,  resize}
